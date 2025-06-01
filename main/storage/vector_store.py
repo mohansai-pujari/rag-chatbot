e4d5bf4020f800fd816466209266f3cb0b9341a3
mohansai_pujari <mohansai.pujari@gmail.com>
2025-06-01 23:50:12 +0530

    python version change

--- main/storage/vector_store.py
+++ main/storage/vector_store.py
@@ -1,13 +1,13 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
 
 class VectorStore:
     def __init__(self):
-        self.embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
         self.store = None
 
     def create_store(self, documents):
-        self.store = FAISS.from_documents(documents, self.embedding_model)
+        from langchain_community.embeddings import HuggingFaceEmbeddings  # lazy import
+        embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        self.store = FAISS.from_documents(documents, embedding_model)
 
     def search(self, query, top_k):
         return self.store.similarity_search(query, k=top_k)
